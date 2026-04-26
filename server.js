4b3716b8e2b3823d3d99334ae3409b1d4684fa31
Photified <justinmainphoto@gmail.com>
2026-04-25 20:22:04 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -160,8 +160,8 @@ setInterval(() => {
 const BIOMES = [
     { name: 'Forest', top: 0x556B2F, trunk: 0x5C4033, leaf: 0x228B22, wall: 0x2E8B57, skyDay: 0x87CEEB, skySunset: 0xFF7E47, skyNight: 0x020211 },
     { name: 'Winter', top: 0xFFFAFA, trunk: 0x8B4513, leaf: 0xFFFFFF, wall: 0x4682B4, skyDay: 0xCAE1FF, skySunset: 0xDDA0DD, skyNight: 0x000033 },
-    { name: 'Neon Arcade', top: 0x818589, trunk: 0x00FFFF, leaf: 0x32CD32, wall: 0x4B0082, skyDay: 0x1A0B2E, skySunset: 0x4B0082, skyNight: 0x050011 }, // Gunmetal grey floor
-    { name: 'Desert', top: 0xEDC9AF, trunk: 0x2E8B57, leaf: 0x228B22, wall: 0xD2B48C, skyDay: 0x87CEEB, skySunset: 0xFF4500, skyNight: 0x000022 } // Candy Land officially replaced
+    { name: 'Neon Arcade', top: 0x818589, trunk: 0x00FFFF, leaf: 0x32CD32, wall: 0x4B0082, skyDay: 0x1A0B2E, skySunset: 0x4B0082, skyNight: 0x050011 },
+    { name: 'Desert', top: 0xD4A373, trunk: 0x2E8B57, leaf: 0x228B22, wall: 0xD2B48C, skyDay: 0x87CEEB, skySunset: 0xFF4500, skyNight: 0x000022 }
 ];
 
 function generateMap() {
@@ -201,7 +201,7 @@ function generateMap() {
             mapBlocks.push({ x: x, y: y + 0.5, z: z, color: currentBiome.top }); 
 
             if (x > -19 && x < 19 && z > -19 && z < 19) {
-                // 12% prop spawn rate - dense but highly optimized
+                // 12% prop spawn rate
                 if (Math.random() < 0.12) { 
                     let type = Math.random();
 
@@ -609,7 +609,7 @@ io.on('connection', (socket) => {
                         io.emit('playerUnstunned', socket.id);
                         io.emit('playerUpsideDown', { id: socket.id, state: false });
                     }
-                }, 5000);
+                }, 10000); // 10 SECOND STUN PENALTY
             } else if (players[socket.id] && players[socket.id].role === 'seeker') {
                 io.emit('playerLavaDeath', socket.id);
             }
